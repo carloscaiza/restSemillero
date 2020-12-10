7efa4de325422f4704174b5652db0abc6dbb220b
carloscaiza <krlos_au@yahoo.com>
2020-12-10 10:21:32 -0500

    Metodos de insertar y actualizar rest ok2

--- src/main/java/com/clearminds/smo/cca/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/smo/cca/servicios/RestEstudiantes.java
@@ -1,9 +1,14 @@
 package com.clearminds.smo.cca.servicios;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.clearminds.cca.dtos.Estudiante;
@@ -39,7 +44,7 @@ public class RestEstudiantes {
 	}
 	
 	@Path("/actualizar")
-	@POST
+	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void actualizar(Estudiante est) throws BDDException{
 		
@@ -54,6 +59,47 @@ public class RestEstudiantes {
 		}
 		
 	}
+	
+//	@Path("/listar")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public ArrayList<Estudiante> listar() throws BDDException{
+//		
+//		System.out.println("Rest listar...");
+//		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+//		
+//		ArrayList<Estudiante> estudiantes = null;				
+//		
+//		try {
+//			estudiantes = srvEstudiante.listarEstudiantes();			
+//		} catch (BDDException e) {
+//			e.printStackTrace();
+//			e.getMessage();
+//		}
+//		return estudiantes;
+//		
+//	}
+	
+//	@Path("/buscar/{idEstudiante}")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Estudiante buscar(@PathParam("idEstudiante") int idEst) throws BDDException{		
+//		
+//		System.out.println("Rest buscar...");
+//		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+//		
+//		Estudiante estudianteEnc = null;				
+//		
+//		try {
+//			estudianteEnc = srvEstudiante.buscarEstudiante(idEst);			
+//		} catch (BDDException e) {
+//			e.printStackTrace();
+//			e.getMessage();
+//		}
+//		return estudianteEnc;
+//		
+//	}
+	
 	
 	@Path("/busqueda")
 	@GET
